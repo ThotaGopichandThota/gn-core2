54e93d37137b639fa0262992af085f8155c4e8ae
Gopichand <tgopichand74@gmail.com>
2024-04-26 17:42:49 +0530

    v0.0.0

---
+++ core/loggingFunctions_test.go
@@ -0,0 +1,30 @@
+package core_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ThotaGopichandThota/gn-core2/core"
+	"github.com/ThotaGopichandThota/gn-core2/core/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDumpGoRoutinesToLogShouldNotPanic(t *testing.T) {
+	t.Parallel()
+
+	defer func() {
+		r := recover()
+		if r != nil {
+			require.Fail(t, fmt.Sprintf("should have not paniced %v", r))
+		}
+	}()
+
+	core.DumpGoRoutinesToLog(0, &mock.LoggerMock{})
+}
+
+func TestGetRunningGoRoutines(t *testing.T) {
+	t.Parallel()
+
+	res := core.GetRunningGoRoutines(&mock.LoggerMock{})
+	require.NotNil(t, res)
+}
